17818911ce36db5baa63e3cf37d109f6e9057bcd
Marcello Borio <ICTS21-23.227@studenti.local>
2022-05-31 19:15:08 +0200

    k

--- app.js
+++ app.js
@@ -1,6 +1,88 @@
-import express from "express";
+import express, { response } from "express";
 import 'dotenv/config';
+import authMiddleware from "./middlewares/auth.middleware.js";
+import logMiddleware from "./middlewares/log.middleware.js";
+import * as fs from "fs";
 
+const port=process.env.port
 const app = express()
+const data=fs.readFileSync('database/githubnotes.json');
+const result=data.toString();
+//console.log(result)
 
-app.listen(process.env.PORT || 3000)
+app.get('/', authMiddleware, function (req, res) {
+
+    console.log(`Faccio modifica senza fare restart`)
+    res.send('notes')
+  })
+  
+  app.get('/test', logMiddleware, (request,response)=> 
+    response
+    .status(200)
+    .json ({
+      'data':'Valore',
+      'status':'Ok',
+      "value": 1,
+    })
+    .response.send(result)
+
+  )
+
+  
+  app.get('/p', logMiddleware, (request,response)=> 
+  response
+  .send(result)
+  
+  
+  
+)
+
+  app.get('/api/notes', (request,response)=> 
+  response
+  .status(200)
+  .json ({
+    "success": true,
+  "list": true,
+  "data": 1
+})
+)
+
+
+app.get('/api/prova', (request,response)=> 
+  response
+  .status(200)
+  .json ({
+    "success": true,
+  "list": true,
+  "data": 1,
+  
+})
+
+)
+
+app.get('/api/notes/:uuid', (request,response)=> 
+    response
+    .status(200)
+    .json ({
+      'data':'Valore',
+      'status':'Ok',
+      "value": 1,
+    })
+  )
+
+
+  //per gestire tutto il resto
+  app.all('*', (request, response)=>{
+    console.log(`sei in una risorsa non corretta`)
+    response
+    .status(500)
+    .json ({
+      'success':'False',
+      'code':'1001',
+      "message": 'Risorsa non disponbiile',
+    })
+  })
+
+
+app.listen(port || 3000)
+
---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,17 @@
+const authMiddleware = (request, response, next) => {
+    console.log("Auth Middleware: Deve controllare se dentro l'header esiste la chiave corretta");
+    const {headers} = request;
+
+    if (headers['secret'] === process.env.API_KEY) {
+        next();
+    } else {
+        response
+            .status(401)
+            .json({
+                status: 'fail',
+                code: 2001,
+                error: 'Unauthorized'
+            });
+    }
+}
+export default authMiddleware
