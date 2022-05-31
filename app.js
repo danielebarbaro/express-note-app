c2d3bb348ffd1c9cbda1b2008001499f7099635e
Stefano Saldana <saldana.stefano@gmail.com>
2022-05-31 17:14:15 +0200

    Scrittura note nel file
    Lavoro fatto in classe, mi da un errore sullo stringify dentro la cartella node_modules

--- app.js
+++ app.js
@@ -1,6 +1,55 @@
 import express from "express";
 import 'dotenv/config';
+import body from 'express-validator'
+import * as fs from "fs";
+import axios from 'axios';
+import logMiddleware from './middlewares/log.middleware,js'
 
 const app = express()
 
-app.listen(process.env.PORT || 3000)
+app.use(express.json())
+
+app.get(
+    '/init',
+    async (req, res) => {
+
+            // https://its.dbdevelopment.tech/notes
+            const options = {
+                method: 'post',
+                url: 'https://its.dbdevelopment.tech/notes',
+                headers: {
+                    "token": "v623qo-882097-cpo"
+                },
+                data: {
+                    "user": "@Wayde2112"
+                }
+            };
+            
+            const notes = await axios
+                .request(options)
+                fs.writeFileSync(`./database/githubnotes.json`, JSON.stringify(notes.data))
+
+        res
+            .status(200)
+            .json({
+                "success": true,
+                "list": true,
+                "data": notes                
+            })
+    }
+)
+
+app.get('/api/notes/:uuid', logMiddleware, function (req, res) {
+    const {uuid} = req.params
+    res
+        .status(200)
+        .const('applications/json')
+        .json({
+            status: 'success',
+            code: 123,
+            data: []
+        })
+})
+
+
+app.listen(process.env.PORT || 5005)
