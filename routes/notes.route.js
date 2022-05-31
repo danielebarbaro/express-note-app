e3107706c10c6ea25c869da2d1d373892ce52e77
simone.oliva <simone.oliva.work@gmail.com>
2022-05-31 21:03:00 +0200

    limit e date funzionano, ma non su path /api/notes

--- routes/notes.route.js
+++ routes/notes.route.js
@@ -44,7 +44,7 @@ router.get('/init', logMiddleware,(req, res) => {
 // });
 
 router.route('/api/notes')
-    .get(logMiddleware,(req, res) => {
+    .get(logMiddleware,async (req, res) => {
 
             res.status(200).json({
                 "success" : true,
@@ -52,45 +52,6 @@ router.route('/api/notes')
                 "data":notesLoader()
             })
         })
-    .get(query('date').trim().isDate(), auth, logMiddleware,async (req, res) => {
-        let date = req.query.date;
-        console.log(date);
-        let filtered = notesLoader().filter(n => new Date(n.date) > new Date(date))
-        console.log()
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            res.status(400).json({
-                success: false,
-                error: errors.array()
-            })
-        }
-        res.status(200).json({
-            "success" : true,
-            "filtered" : true,
-            "data" : [filtered]
-        })
-    })
-    .get(query('limit').trim().isInt(), auth, logMiddleware,async (req, res) =>{
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            res.status(400).json({
-                success: false,
-                error: errors.array()
-            })
-        }
-        let limit = req.query.limit;
-        console.log(limit)
-        let orderedNotes = notesLoader().sort((a, b) => { new Date(a.date) > new Date(b.date) });
-        let limitedNotes = [];
-        for (let i = orderedNotes.length ; i > limit ; i--){
-            limitedNotes.push(orderedNotes[i]);
-        }
-        response.status(200).json({
-            success: true,
-            data : limitedNotes
-        })
-    
-    })
     .post(auth, logMiddleware,(req, res) => {
 
         let newUUID = generateUUID()
@@ -120,6 +81,45 @@ router.route('/api/notes')
             "title" : newTitle,
             "body" : newBody
         })
+    })
+
+router.route('/api/notes/date')
+    .get(query('date').trim().isDate(), auth, logMiddleware, (req, res) => {
+        let date = req.query.date;
+        console.log(date);
+        let filtered = notesLoader().filter(n => new Date(n.date) > new Date(date))
+        console.log()
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.status(400).json({
+                success: false,
+                error: errors.array()
+            })
+        }
+        res.status(200).json({
+            "success" : true,
+            "filtered" : true,
+            "data" : [filtered]
+        })
+    })
+router.get('/api/notes/limit',query('limit').isNumeric(), auth, logMiddleware, (req, res) =>{
+        const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                res.status(400).json({
+                    success: false,
+                    error: errors.array()
+                })
+            }
+        let limit = req.query.limit;
+        console.log(limit)
+        let orderedNotes = notesLoader().sort((a,b) =>{
+            new Date(b.date) - new Date(a.date)
+        });
+        res.status(200).json({
+            success: true,
+            data : orderedNotes.slice(-limit)
+        })
+
     })
 
 router.route('/api/notes/:uuid')
@@ -182,144 +182,5 @@ router.route('/api/notes/:uuid')
         })
     
     })
-
-//funziona
-// router.get(`/api/notes/:uuid`,param('uuid').isLength({min: 36, max:36}), logMiddleware,(req, res, err) => {
-//     let uuid = req.params.uuid;
-//     const errors = validationResult(req);
-//     let listNotes = notesLoader();
-//     if(!errors.isEmpty()){
-//         res.status(400).json({
-//             success: false,
-//             error: errors.array()
-//         })
-//     }
-//     res.status(200)
-//         .json({
-//         "success" : true,
-//         "single": true,
-//         "data": listNotes.filter(l => l.id === uuid)
-//     });
-        
-// });
-
-// //non va
-// router.get('/api/notes', query('date').trim().isDate(), auth, logMiddleware,async (req, res) => {
-//     let date = req.query.date;
-//     console.log(date);
-//     let filtered = notesLoader().filter(n => new Date(n.date) > new Date(date))
-//     console.log()
-//     const errors = validationResult(req);
-//     if(!errors.isEmpty()){
-//         res.status(400).json({
-//             success: false,
-//             error: errors.array()
-//         })
-//     }
-//     res.status(200).json({
-//         "success" : true,
-//         "filtered" : true,
-//         "data" : [filtered]
-//     });
-// });
-
-// //non va
-// router.get(`/api/notes`, query('limit').trim().isInt(), auth, logMiddleware,async (req, res) =>{
-//     const errors = validationResult(req);
-//     if(!errors.isEmpty()){
-//         res.status(400).json({
-//             success: false,
-//             error: errors.array()
-//         })
-//     }
-//     let limit = req.query.limit;
-//     console.log(limit)
-//     let orderedNotes = notesLoader().sort((a, b) => { new Date(a.date) > new Date(b.date) });
-//     let limitedNotes = [];
-//     for (let i = orderedNotes.length ; i > limit ; i--){
-//         limitedNotes.push(orderedNotes[i]);
-//     }
-//     response.status(200).json({
-//         success: true,
-//         data : limitedNotes
-//     })
-    
-// });
-
-//funziona e autentica
-// router.post(`/api/notes`, auth, logMiddleware,(req, res) => {
-
-//     let newUUID = generateUUID()
-//     let newUser = req.body.user;
-//     let newDate = req.body.date;
-//     let newTitle = req.body.title;
-//     let newBody = req.body.body;
-//     let noteArray = {
-//         "id": newUUID,
-//         "user" : newUser,
-//         "date" : newDate,
-//         "title" : newTitle,
-//         "body" : newBody
-//     }
-
-//     let oldNotes = notesLoader()
-    
-    
-//     oldNotes.push(noteArray)
-    
-//     saveNote(oldNotes)
-
-//     res.status(201).json({
-//         "id": newUUID,
-//         "user" : newUser,
-//         "date" : newDate,
-//         "title" : newTitle,
-//         "body" : newBody
-//     })
-// });
-
-// //funziona e autentica
-// router.put(`/api/notes/:uuid`,param('uuid').isLength({min: 36, max:36}), auth, logMiddleware,(req, res) => {
-
-//     let oldNotes = notesLoader();
-//     let uuid = req.params.uuid;
-//     let title = req.body.title;
-//     let body = req.body.body;
-//     let selectedNote = oldNotes.find(note => note.id === uuid)
-//     //console.log(selectedNote);
-//     const errors = validationResult(req);
-//     if(!errors.isEmpty()){
-//         res.status(400).json({
-//             success: false,
-//             error: errors.array()
-//         })
-//     }
-    
-//     let editedNote;
-
-//     if(selectedNote === undefined){
-//         res.status(404).json({
-//             "success" : false,
-//             "error": "Nota non trovata" 
-//         })
-//     }else{
-//         editedNote = {
-//             "id": uuid,
-//             "user" : selectedNote.user,
-//             "date" : selectedNote.date,
-//             "title" : title,
-//             "body" : body,
-//             "created_at": selectedNote.created_at
-//         }
-//     }
-//     let editedOldNotes = oldNotes.filter( n => n.id !== uuid);
-//     editedOldNotes.push(editedNote);
-//     saveNote(editedOldNotes)
-//     res.status(200).json({
-//         "title" : editedNote.title,
-//         "body": editedNote.body
-//     })
-
-// });
 
 export default router;
