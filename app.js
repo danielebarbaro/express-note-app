3318bb5c32c9bdedb31803c3200e1408a4c60597
LorenzoGParisi <lorenzo.parisi@edu.itspiemonte.it>
2022-06-05 22:22:47 +0200

    3

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 import express from "express";
 import 'dotenv/config';
 
+const port = process.env.port
 const app = express()
 
-app.listen(process.env.PORT || 3000)
+app.get('/', function (req, res) {
+    res.header("Content-Type",'application/json');
+    res.json()
+  })
+
+  app.all('*', (request, response)=>{
+    console.log(`sei in una risorsa non corretta`)
+    response
+    .status(500)
+    .json ({
+      'success':'False',
+      'code':'1001',
+      "message": 'Risorsa non disponbiile',
+    })
+  })
+
+app.listen(process.env.port || 3000)
