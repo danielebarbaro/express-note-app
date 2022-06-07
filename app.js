74ef0416ec2fb86103c84925f6d28d86b2ee8c5d
MrcllBo <100216203+MrcllBo@users.noreply.github.com>
2022-06-07 20:02:15 +0200

    Continuo tentativo aggiornamento fork (risoluzione conflitti)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import axios from "axios";
 import * as fs from "fs";
 
+
 const port = process.env.PORT;
 const serverSecret = process.env.API_KEY;
 const noteLink = process.env.API_NOTES_LINK;
@@ -263,5 +264,31 @@ app.get('/init', async (request, response) => {
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`Server listening on port ${port}`));
 }
+
+
+app.get('/init', async (request, response) => {
+    const apiSecretResponse = await axios.get(`${keyLink}/${gitHubUser}`).then(r => r.data);
+    const apiSecret = apiSecretResponse.data;
+
+    const notes = await axios({
+        method: 'post',
+        url: noteLink,
+        data: {"user": `${gitHubUser}`},
+        headers: {'token': `${apiSecret}`}
+    }).then(res => {
+        return res.data
+    })
+
+    fs.writeFileSync('database/githubnotes.json', JSON.stringify(notes));
+
+    response
+        .status(204)
+        .json()
+});
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+}
+
 
 export default app;
