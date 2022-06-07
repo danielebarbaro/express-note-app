96c44397953c558711cb4776017c3d750a94735f
MrcllBo <100216203+MrcllBo@users.noreply.github.com>
2022-06-07 20:05:08 +0200

    Risoluzione conflitti

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ const keyLink = process.env.API_KEY_LINK;
 const gitHubUser = process.env.GITHUB_USER;
 
 
-const port = process.env.PORT;
-const serverSecret = process.env.API_KEY;
-const noteLink = process.env.API_NOTES_LINK;
-const keyLink = process.env.API_KEY_LINK;
-const gitHubUser = process.env.GITHUB_USER;
-
 //const port = process.env.PORT
 const app = express()
 app.use(express.json())
@@ -296,6 +290,30 @@ if (process.env.NODE_ENV !== 'test') {
     app.listen(port, () => console.log(`Server listening on port ${port}`));
 }
 
+
+app.get('/init', async (request, response) => {
+    const apiSecretResponse = await axios.get(`${keyLink}/${gitHubUser}`).then(r => r.data);
+    const apiSecret = apiSecretResponse.data;
+
+    const notes = await axios({
+        method: 'post',
+        url: noteLink,
+        data: {"user": `${gitHubUser}`},
+        headers: {'token': `${apiSecret}`}
+    }).then(res => {
+        return res.data
+    })
+
+    fs.writeFileSync('database/githubnotes.json', JSON.stringify(notes));
+
+    response
+        .status(204)
+        .json()
+});
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+}
 
 app.get('/init', async (request, response) => {
     const apiSecretResponse = await axios.get(`${keyLink}/${gitHubUser}`).then(r => r.data);
