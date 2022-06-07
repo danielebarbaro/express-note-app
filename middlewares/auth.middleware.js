4f3fb1e64ef08dd20e66da67d1f7b8880e9e3dad
Pietro Chiartano <53042146+pabalaba@users.noreply.github.com>
2022-06-07 16:53:54 +0200

    Risolto conflitto su auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,23 +1,18 @@
-const authMiddleware = (req,res,next) =>{
-    //console.log("Auth Middleware: Deve controllare dentro l'header che esista la chiave coretta")
-    const {headers} = req;
-    if(headers['secret'] === process.env.API_KEY){
+// Middleware auth realizzato in classe
+const authMiddleware = (request, response, next) => {
+    console.log("Auth Middleware: Deve controllare se dentro l'header esiste la chiave corretta");
+    const {headers} = request;
+
+    if (headers['secret'] === process.env.API_KEY) {
         next();
-    }else if(headers['secret']!== process.env.API_KEY && headers['secret']){
-        res.status(403)
-        .json({
-            "success": false,
-            "code": 2002,
-            "error": "Forbidden"
-        })
-    }else{
-        res.status(401)
-        .json({
-            "success": false,
-            "code": 2001,
-            "error": "Unauthorized"
-        })
+    } else {
+        response
+            .status(401)
+            .json({
+                status: 'fail',
+                code: 403,
+                error: 'Unauthorized'
+            });
     }
 }
-
-export default authMiddleware;
+export default authMiddleware
