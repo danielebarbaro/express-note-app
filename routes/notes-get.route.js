eecf466dd59135605e48812c0cb8f6f7701f8227
D4n13l3Q <daniele.quasimodo@edu.itspiemonte.it>
2022-06-10 11:53:51 +0200

    Rimosso "created_at" dove non richiesto

--- routes/notes-get.route.js
+++ routes/notes-get.route.js
@@ -1,18 +1,38 @@
+//IMPORTS
+
+// Packages
 import express from 'express';
 import { query, param, validationResult } from 'express-validator';
 
 // Notes
 import * as core from '../core-notes.js';
 
+// Middlewares
 import logMiddleware from '../middlewares/log.middleware.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 
 
 
+
+// HELPERS
+const filterObject = function ( obj, ...propertiesToRemove ) { // Parses "obj" and returns a deep copy keeping only the properties not listed in "propertiesToRemove".
+    const newEntries = [];
+	Object.entries(obj).forEach(
+	    function ([ key, val ]) {
+            if (!propertiesToRemove.includes(key)) newEntries.push([ key, val ])
+        }
+    );
+	return Object.fromEntries(newEntries);
+}
+
+
+
+// SETUP
 const notesGetRoute = express.Router();
 
 
 
+// ROUTES
 notesGetRoute.get(
     '/',
     function (request, reply, next) {
@@ -43,32 +63,32 @@ notesGetRoute.get(
            return reply.status(400).json({ errors: errors.array() });
         }
         
-        let filtered = false;
-        
-        let limit = -1;
-        let sortByDate = false;
-        
-        if (request.query.limit !== undefined) {
-            limit = request.query.limit;
-            sortByDate = true;
-            filtered = true;
-        }
-        
-        let afterDate = undefined;
-        
-        if (request.query.date !== undefined) {
-            afterDate = request.query.date;
-            filtered = true;
-        }
+		let filtered = false;
+		
+		let limit = -1;
+		let sortByDate = false;
+		
+		if (request.query.limit !== undefined) {
+			limit = request.query.limit;
+			sortByDate = true;
+			filtered = true;
+		}
+		
+		let afterDate = undefined;
+		
+		if (request.query.date !== undefined) {
+			afterDate = request.query.date;
+			filtered = true;
+		}
         
         //const notes = request.app.locals.noteboard.listNotes(limit, sortByDate, afterDate);
         const notes = core.listNotes(request.app.locals.noteboard, limit, sortByDate, afterDate);
         
         reply.status(200).json({
             success: true,
-            filtered: filtered,
+			filtered: filtered,
             list: true,
-            data: notes
+            data: filtered ? notes.map( note => filterObject(note, 'created_at') ) : notes
         });
     }
 );
@@ -92,6 +112,8 @@ notesGetRoute.get(
         //const note = request.app.locals.noteboard.getNote(request.params.uuid);
         const note = core.getNote(request.app.locals.noteboard, request.params.uuid);
         
+		const {created_at, ...strippedNote} = note;
+		
         if (note === undefined) {
             reply.sendStatus(404);
         } else {
@@ -99,7 +121,7 @@ notesGetRoute.get(
                 success: true,
                 single: true,
                 data: [
-                    note
+                    filterObject(note, 'created_at')
                 ]
             });
         }
@@ -109,4 +131,5 @@ notesGetRoute.get(
 
 
 
+// EXPORTS
 export default notesGetRoute;
